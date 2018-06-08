7a02cc433b89893a8443cb4d5ce6be8e48fdbadf
peter <peter@soez.io>
2018-06-08 09:29:56 +0800

    添加上传图片接口

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//加载ueditor 模块  
+var ueditor = require("ueditor");
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -18,15 +21,41 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+//使用模块  
+app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function (req, res, next) {
+  // ueditor 客户发起上传图片请求  
+  if (req.query.action === 'uploadimage') {
+    var foo = req.ueditor;
+
+    var imgname = req.ueditor.filename;
+
+    var img_url = '/images/ueditor/';
+    res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做  
+    res.setHeader('Content-Type', 'text/html'); //IE8下载需要设置返回头尾text/html 不然json返回文件会被直接下载打开  
+  }
+  //  客户端发起图片列表请求  
+  else if (req.query.action === 'listimage') {
+    var dir_url = '/images/ueditor/';
+    res.ue_list(dir_url); // 客户端会列出 dir_url 目录下的所有图片  
+  }
+  // 客户端发起其它请求  
+  else {
+    // console.log('config.json')  
+    res.setHeader('Content-Type', 'application/json');
+    res.redirect('/ueditor/nodejs/config.json');
+  }
+}));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -37,7 +66,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -48,7 +77,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
